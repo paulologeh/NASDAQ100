c225d7fb39c3f4f784bd82f3c78245a9c24a3721
Paul Ologeh <paulologeh@Paul-Ologehs-MacBook-Air.local>
2021-08-20 14:42:26 +0100

    fixed broken scrapper

--- marketdata.py
+++ marketdata.py
@@ -13,9 +13,9 @@ class Marketdata:
         pass
 
     def scrapeYahoo(self, symbol, classValue, reactid, classType='span'):
-        url = 'https://finance.yahoo.com/quote/{}?p={}&.tsrc=fin-srch'.format(
-            symbol, classValue)
+        url = f'https://finance.yahoo.com/quote/{symbol}?p={symbol}&.tsrc=fin-srch'
         response = requests.get(url, timeout=5)
+
         soup = BeautifulSoup(response.text, 'html.parser')
         Object = soup.find(classType, attrs={
                            'class': classValue, 'data-reactid': reactid})
@@ -33,17 +33,17 @@ class Marketdata:
         return text
 
     def getPrice(self, Symbol):
-        # <span class = "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)" data-reactid = "32" > 79.01 < /span >
-        return self.scrapeYahoo(Symbol, "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)", "32")
+        # <span class = "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)" data-reactid = "49" > 2,713.60 </span>
+        return self.scrapeYahoo(Symbol, "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)", "49")
 
     def getDaysHigh(self, Symbol):
-        # <td class = "Ta(end) Fw(600) Lh(14px)" data-test = "DAYS_RANGE-value" data-reactid = "117" > 75.68 - 79.74 < /td >
-        return self.scrapeYahoo(Symbol, "Ta(end) Fw(600) Lh(14px)", "117", 'td')
+        # <td class = "Ta(end) Fw(600) Lh(14px)" data-test = "DAYS_RANGE-value" data-reactid = "114" > 75.68 - 79.74 < /td >
+        return self.scrapeYahoo(Symbol, "Ta(end) Fw(600) Lh(14px)", "114", 'td')
 
     def get52WeekHigh(self, Symbol):
         # <td class = "Ta(end) Fw(600) Lh(14px)" data-test = "FIFTY_TWO_WK_RANGE-value" data-reactid = "121" > 1, 626.03 - 3, 552.25 < /td >
-        return self.scrapeYahoo(Symbol, "Ta(end) Fw(600) Lh(14px)", "121", 'td')
+        return self.scrapeYahoo(Symbol, "Ta(end) Fw(600) Lh(14px)", "118", 'td')
 
     def getPreviousClose(self, Symbol):
-        # <span class="Trsdu(0.3s) " data-reactid="98">83.45</span>
-        return self.scrapeYahoo(Symbol, "Trsdu(0.3s)", "98")
+        # <<span class="Trsdu(0.3s) " data-reactid="97">2,708.98</span>
+        return self.scrapeYahoo(Symbol, "Trsdu(0.3s)", "97")
